f80697765236498e0612de8c31ecfcb208516b44
DeepPealer <pratonibratoni@gmail.com>
2025-11-19 15:36:32 +0100

    fix: Update DAG file

--- dags/pagila_test.py
+++ dags/pagila_test.py
@@ -10,13 +10,13 @@ default_args = {
 with DAG(
     dag_id='test_pagila_connection',
     default_args=default_args,
-    schedule_interval=None, # Запуск только вручную
+    schedule_interval=None,
     catchup=False,
     tags=['pagila']
 ) as dag:
 
     count_films = PostgresOperator(
         task_id='count_films',
-        postgres_conn_id='postgres_pagila_conn', # Это ID подключения, создадим его на след. шаге
+        postgres_conn_id='postgres_pagila_conn',
         sql='SELECT COUNT(*) FROM public.film;'
     )
